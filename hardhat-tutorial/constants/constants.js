6d9272238cc801b20ef7714a501e85310c69abb5
Morten Bergseng <bergseng.m@gmail.com>
2022-04-27 14:24:29 +0200

    first

---
+++ hardhat-tutorial/constants/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+    "0xC98d1a9C748A638f49219a437830566f158070d9";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
